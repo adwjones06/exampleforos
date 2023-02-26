16cce77b1bf9500d975f2bf62b29b37ba31688d9
adwjones06 <70573218+adwjones06@users.noreply.github.com>
2023-02-26 14:00:42 +0000

    Add files via upload
    This is us uploading version 1 of a piece of code to an empty repository.

---
+++ functions.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+	// This pointer (ptr) will set aside the heaping location 
+	int* ptr;
+	int n, i;
+
+	// Get the number of elements for the array
+	printf("Enter number of elements:");
+	scanf("%d",&n);
+	printf("Entered number of elements: %d\n", n);
+
+	// Dynamically allocate memory ("heap) using the malloc(). what is allocated is the array 
+	ptr = (int*)malloc(n * sizeof(int));
+
+	// Check if the memory has been successfully heaped
+	if (ptr == NULL) {
+		printf("Memory not allocated.\n");
+		exit(0);
+	}
+	else {
+
+		// Memory has been successfully allocated
+		printf("Memory successfully allocated using malloc.\n");
+
+		// Get the elements of the array
+		for (i = 0; i < n; ++i) {
+			ptr[i] = i + 1;
+		}
+
+		// Print the elements of the array
+		printf("The elements of the array are: ");
+		for (i = 0; i < n; ++i) {
+			printf("%d, ", ptr[i]);
+		}
+	}
+
+	return 0;
+}
